9219105750c9f4172dc4fa2c47c747ae93dcd2fb
Akhil <skippynose2@users.noreply.github.com>
2024-04-22 16:57:38 -0400

    Add files via upload

--- realsense_depth.py
+++ realsense_depth.py
@@ -14,7 +14,7 @@ class DepthCamera:
         device_product_line = str(device.get_info(rs.camera_info.product_line))
 
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        # config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 
 
@@ -24,13 +24,13 @@ class DepthCamera:
     def get_frame(self):
         frames = self.pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+        # color_frame = frames.get_color_frame()
 
         depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        if not depth_frame or not color_frame:
+        # color_image = np.asanyarray(color_frame.get_data())
+        if not depth_frame:     # or not color_frame:
             return False, None, None
-        return True, depth_image, color_image
+        return True, depth_image, # color_image
 
     def release(self):
         self.pipeline.stop()
--- testing.py
+++ testing.py
@@ -20,16 +20,16 @@ cv2.namedWindow("Color frame")
 cv2.setMouseCallback("Color frame", show_distance)
 
 while True:
-    ret, depth_frame, color_frame = dc.get_frame()
+    ret, depth_frame = dc.get_frame()
 
     # Show distance for a specific point
-    cv2.circle(color_frame, point, 4, (0, 0, 255))
+    # cv2.circle(color_frame, point, 4, (0, 0, 255))
     distance = depth_frame[point[1], point[0]]
 
-    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+    #cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
     cv2.imshow("depth frame", depth_frame)
-    cv2.imshow("Color frame", color_frame)
+    # cv2.imshow("Color frame", color_frame)
     key = cv2.waitKey(1)
     if key == 27:
         break
